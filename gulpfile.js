4469c51a97bf5655a30326448fcd2d3941c850ce
Shawn Jackson <shawn@resgrid.com>
2016-03-05 22:41:55 -0800

    Working on the Azure deployment gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,10 @@ var gulp = require('gulp'),
     http = require('http'),
     ecstatic = require('ecstatic'),
     shell = require('gulp-shell'),
+    runSequence = require("run-sequence"),
+    insertLines = require('gulp-insert-lines'),
     print = require('gulp-print');
+
 
 var watchGlob = 'www/**/*.{js,html,css}';
 
@@ -63,6 +66,73 @@ gulp.task("plugins", ["git-check"], function(cb) {
         });
 
     }, cb);
+});
+
+// Builds and copies the files need to run the app in the web
+gulp.task("web:build", function (cb) {
+	runSequence("clean:web", "web:copy", "web:addHeader", cb);
+});
+
+/**
+ * Removes the web directory.
+ */
+gulp.task("clean:web", function () {
+	return del([
+		"web"
+	]);
+});
+
+// Builds and copies the files need to run the app in the web
+gulp.task("web:copy", function (cb) {
+
+	gulp.src("./www/css/**")
+			.pipe(gulp.dest("web/css"))
+			.on("end", function () {
+
+				gulp.src("./www/img/**")
+						.pipe(gulp.dest("web/img"))
+						.on("end", function () {
+
+							gulp.src("./www/js/**")
+									.pipe(gulp.dest("web/js"))
+									.on("end", function () {
+
+										gulp.src("./www/templates/**")
+												.pipe(gulp.dest("web/templates"))
+												.on("end", function () {
+
+													gulp.src("./www/views/**")
+															.pipe(gulp.dest("web/views"))
+															.on("end", function () {
+
+																gulp.src("./www/widgets/**")
+																		.pipe(gulp.dest("web/widgets"))
+																		.on("end", function () {
+
+																			gulp.src("./www/index.html")
+																				.pipe(gulp.dest("web"))
+																				.on("end", function () {
+
+																					gulp.src("./assets/web/icons/**")
+																						.pipe(gulp.dest("web"))
+																						.on("end", cb);
+																				});
+																		});
+															});
+												});
+									});
+						});
+			});
+});
+
+// Adds web specific items to the index.html's header
+gulp.task("web:addHeader", function () {
+	return gulp.src('web/index.html')
+	.pipe(insertLines({
+		'before': /<\/head>$/,
+		'lineBefore': '<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png"><link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png"><link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png"><link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png"><link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png"><link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png"><link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png"><link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png"><link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png"><link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32"><link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192"><link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96"><link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16"><link rel="manifest" href="/manifest.json"><link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"><meta name="msapplication-TileColor" content="#da532c"><meta name="msapplication-TileImage" content="/mstile-144x144.png"><meta name="theme-color" content="#ffffff">'
+	}))
+	.pipe(gulp.dest('web'));
 });
 
 
