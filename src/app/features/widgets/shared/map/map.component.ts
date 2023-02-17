43e5b1c9a1a6a671baf666db7e58037a2edc5848
Shawn Jackson <shawn@resgrid.com>
2023-02-16 20:03:40 -0800

    #8669cmyj9 Updated map to include realtime gps

--- src/app/features/widgets/shared/map/map.component.ts
+++ src/app/features/widgets/shared/map/map.component.ts
@@ -1,6 +1,19 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
-import { MapDataAndMarkersData } from '@resgrid/ngx-resgridlib';
+import {
+  ConnectionState,
+  Consts,
+  EventsService,
+  MapDataAndMarkersData,
+  MappingService,
+  RealtimeGeolocationService,
+} from '@resgrid/ngx-resgridlib';
 import { Observable, Subscription, take } from 'rxjs';
 import { MapWidgetSettings } from 'src/app/models/mapWidgetSettings';
 import {
@@ -17,6 +30,7 @@ import * as WidgetsActions from '../../actions/widgets.actions';
 import { environment } from 'src/environments/environment';
 import { Widget } from 'src/app/models/widget';
 import { SettingsState } from 'src/app/features/settings/store/settings.store';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-widgets-map',
@@ -24,17 +38,35 @@ import { SettingsState } from 'src/app/features/settings/store/settings.store';
   styleUrls: ['./map.component.scss'],
 })
 export class MapWidgetComponent implements OnInit, OnDestroy {
+  @ViewChild('map') mapContainer;
   public widgetsState$: Observable<WidgetsState>;
   public widgetSettingsState$: Observable<MapWidgetSettings | null>;
   public mapDataState$: Observable<MapDataAndMarkersData | null>;
   public settingsState$: Observable<SettingsState | null>;
   public widgetLayoutState$: Observable<Widget[]>;
   private subs = new SubSink();
-  @ViewChild('map') mapContainer;
-  public map: any;
-  public markers: any[];
 
-  constructor(private store: Store<WidgetsState>) {
+  public map: any;
+  public layerControl: any;
+  public baseMaps: any;
+  public markers: any[] = [];
+  public layers: any[] = [];
+  public showCalls: boolean = true;
+  public showStations: boolean = true;
+  public showUnits: boolean = true;
+  public showPersonnel: boolean = true;
+  public hideLabels: boolean = false;
+  public filterText: string = '';
+  public updateDate: string = '';
+  private signalRStarted: boolean = false;
+
+  constructor(
+    private store: Store<WidgetsState>,
+    private mapProvider: MappingService,
+    private realtimeGeolocationService: RealtimeGeolocationService,
+    private events: EventsService,
+    private consts: Consts
+  ) {
     this.widgetsState$ = this.store.select(selectWidgetsState);
     this.widgetSettingsState$ = this.store.select(selectMapWidgetSettingsState);
     this.mapDataState$ = this.store.select(selectMapWidgetDataState);
@@ -44,9 +76,13 @@ export class MapWidgetComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    const date = new Date();
+    this.updateDate = date.toString();
+
     this.subs.sink = this.mapDataState$.subscribe((mapData) => {
       if (mapData) {
         this.processMapData(mapData);
+        this.startSignalR();
       }
     });
 
@@ -67,85 +103,254 @@ export class MapWidgetComponent implements OnInit, OnDestroy {
     }
   }
 
+  private clearLayers() {
+    if (this.layers && this.layers.length > 0) {
+      this.layers.forEach((layer) => {
+        try {
+          this.layerControl.removeLayer(layer);
+          this.map.removeLayer(layer);
+        } catch (error) {}
+      });
+      this.layers = [];
+    } else {
+      this.layers = [];
+    }
+  }
+
+  public colorlayer(feature, layer) {
+    layer.on('mouseover', function (e) {
+      layer.setStyle({
+        fillOpacity: 0.4,
+      });
+    });
+    layer.on('mouseout', function (e) {
+      layer.setStyle({
+        fillOpacity: 0,
+      });
+    });
+  }
+
+  private getMapLayers() {
+    this.mapProvider
+      .getMayLayers(0)
+      .pipe(take(1))
+      .subscribe((data) => {
+        if (data && data.Data && data.Data.LayerJson && this.layerControl) {
+          this.clearLayers();
+
+          var jsonData = JSON.parse(data.Data.LayerJson);
+          if (jsonData) {
+            jsonData.forEach((json) => {
+              var myLayer = L.geoJSON(json, {
+                //onEachFeature: this.colorlayer,
+                style: {
+                  color: json.features[0].properties.color,
+                  opacity: 0.7,
+                  fillColor: json.features[0].properties.color,
+                  //fillColor: json.features[0].properties.fillColor,
+                  fillOpacity: 0.1,
+                },
+              }); //.addData(json);//.addTo(this.map);
+              this.layerControl.addOverlay(
+                myLayer,
+                json.features[0].properties.name
+              );
+
+              this.layers.push(myLayer);
+              //myLayer.addData(json);
+            });
+          }
+        }
+      });
+  }
+
   private processMapData(data: MapDataAndMarkersData) {
     this.settingsState$.pipe(take(1)).subscribe((settings) => {
       if (data && settings && settings.appSettings && settings.appSettings.MapUrl) {
         if (!this.map && this.mapContainer && this.mapContainer.nativeElement) {
+          var osm = L.tileLayer(
+            settings?.appSettings.MapUrl,
+            {
+              crossOrigin: true,
+            }
+          )
+          
+          this.baseMaps = {
+            OpenStreetMap: osm,
+          };
+
           this.map = L.map(this.mapContainer.nativeElement, {
             dragging: false,
             doubleClickZoom: false,
             zoomControl: false,
             attributionControl: false,
+            layers: [osm],
           });
 
-          L.tileLayer(
-            settings?.appSettings.MapUrl,
-            {
-              crossOrigin: true,
-            }
-          ).addTo(this.map);
-
           L.control.attribution({ position: 'bottomleft' }).addTo(this.map);
+          this.layerControl = L.control.layers(this.baseMaps).addTo(this.map);
         }
 
-        if (this.map) {
-          this.map.setView([data.CenterLat, data.CenterLon], data.ZoomLevel);
+        const date = new Date();
+        this.updateDate = date.toString();
+        this.clearLayers();
 
-          // clear map markers
-          if (this.markers && this.markers.length >= 0) {
-            for (var i = 0; i < this.markers.length; i++) {
-              if (this.markers[i]) {
-                this.map.removeLayer(this.markers[i]);
-              }
+        //this.mapProvider.setMarkersForMap(this.map);
+
+        //this.setMapBounds();
+
+        //if (this.map) {
+        this.map.setView([data.CenterLat, data.CenterLon], data.ZoomLevel);
+        //}
+
+        // clear map markers
+        if (this.markers && this.markers.length >= 0) {
+          for (var i = 0; i < this.markers.length; i++) {
+            if (this.markers[i]) {
+              this.map.removeLayer(this.markers[i]);
             }
-
-            this.markers = new Array<any>();
           }
 
-          if (data.MapMakerInfos && data.MapMakerInfos.length > 0) {
-            if (data && data.MapMakerInfos) {
-              data.MapMakerInfos.forEach((markerInfo) => {
-                let marker = L.marker(
-                  [markerInfo.Latitude, markerInfo.Longitude],
-                  {
-                    icon: L.icon({
-                      iconUrl:
-                        'assets/images/mapping/' +
-                        markerInfo.ImagePath +
-                        '.png',
-                      iconSize: [32, 37],
-                      iconAnchor: [16, 37],
-                    }),
-                    draggable: false,
-                    title: markerInfo.Title,
-                    //tooltip: markerInfo.Title,
+          this.markers = new Array<any>();
+        }
+
+        if (data.MapMakerInfos && data.MapMakerInfos.length > 0) {
+          if (data && data.MapMakerInfos) {
+            data.MapMakerInfos.forEach((markerInfo) => {
+              if (
+                this.filterText === '' ||
+                markerInfo.Title.toLowerCase().indexOf(
+                  this.filterText.toLowerCase()
+                ) >= 0
+              ) {
+                let markerTitle = '';
+                let marker: any = null;
+                if (!this.hideLabels) markerTitle = markerInfo.Title;
+
+                if (
+                  (markerInfo.Type == 0 && this.showCalls) ||
+                  (markerInfo.Type == 1 && this.showUnits) ||
+                  (markerInfo.Type == 2 && this.showStations)
+                ) {
+                  if (!this.hideLabels) {
+                    marker = L.marker(
+                      [markerInfo.Latitude, markerInfo.Longitude],
+                      {
+                        icon: L.icon({
+                          iconUrl:
+                            '/assets/images/mapping/' + markerInfo.ImagePath + '.png',
+                          iconSize: [32, 37],
+                          iconAnchor: [16, 37],
+                        }),
+                        draggable: false,
+                        title: markerTitle,
+                      }
+                    )
+                      .bindTooltip(markerTitle, {
+                        permanent: true,
+                        direction: 'bottom',
+                      })
+                      .addTo(this.map);
+
+                    marker.elementId = markerInfo.Id;
+                  } else {
+                    marker = L.marker(
+                      [markerInfo.Latitude, markerInfo.Longitude],
+                      {
+                        icon: L.icon({
+                          iconUrl:
+                            '/assets/images/mapping/' + markerInfo.ImagePath + '.png',
+                          iconSize: [32, 37],
+                          iconAnchor: [16, 37],
+                        }),
+                        draggable: false,
+                        title: markerTitle,
+                      }
+                    ).addTo(this.map);
+
+                    marker.elementId = markerInfo.Id;
                   }
-                )
-                  .bindTooltip(markerInfo.Title, {
-                    permanent: true,
-                    direction: 'bottom',
-                  })
-                  .addTo(this.map);
+                }
 
-                this.markers.push(marker);
-              });
-            }
+                if (marker) {
+                  this.markers.push(marker);
+                }
+              }
+            });
+          }
 
+          if (this.markers && this.markers.length > 0) {
             var group = L.featureGroup(this.markers);
             this.map.fitBounds(group.getBounds());
           }
         }
-
-        let that = this;
-        setTimeout(function () {
-          //window.dispatchEvent(new Event('resize'));
-          //that.map.invalidateSize.bind(that.map)
-
-          if (that.map) {
-            that.map.invalidateSize();
-          }
-        }, 500);
       }
+
+      this.getMapLayers();
     });
+  }
+
+  public startSignalR() {
+    if (!this.signalRStarted) {
+      Object.defineProperty(WebSocket, 'OPEN', { value: 1 });
+      this.realtimeGeolocationService.connectionState$.subscribe(
+        (state: ConnectionState) => {
+          if (state === ConnectionState.Disconnected) {
+            //this.realtimeGeolocationService.restart(this.departmentId);
+          }
+        }
+      );
+
+      this.signalrInit();
+      this.realtimeGeolocationService.start();
+
+      this.signalRStarted = true;
+    }
+  }
+
+  public stopSignalR() {
+    this.realtimeGeolocationService.stop();
+    this.signalRStarted = false;
+  }
+
+  private signalrInit() {
+    this.events.subscribe(
+      this.consts.SIGNALR_EVENTS.PERSONNEL_LOCATION_UPDATED,
+      (data: any) => {
+        console.log('person location updated event');
+        if (data) {
+          let personMarker = _.find(this.markers, [
+            'elementId',
+            `p${data.userId}`,
+          ]);
+
+          if (personMarker) {
+            const date = new Date();
+            this.updateDate = date.toString();
+
+            personMarker.setLatLng([data.latitude, data.longitude]);
+          }
+        }
+      }
+    );
+    this.events.subscribe(
+      this.consts.SIGNALR_EVENTS.UNIT_LOCATION_UPDATED,
+      (data: any) => {
+        console.log('unit location updated event');
+        if (data) {
+          let unitMarker = _.find(this.markers, [
+            'elementId',
+            `u${data.unitId}`,
+          ]);
+
+          if (unitMarker) {
+            const date = new Date();
+            this.updateDate = date.toString();
+
+            unitMarker.setLatLng([data.latitude, data.longitude]);
+          }
+        }
+      }
+    );
   }
 }
--- src/assets/env.prod.js
+++ src/assets/env.prod.js
@@ -7,11 +7,6 @@
     window['env']['channelUrl'] = '${CHANNEL_URL}';
     window['env']['channelHubName'] = '${CHANNEL_HUB_NAME}';
     window['env']['logLevel'] = '${LOG_LEVEL}';
-    window['env']['what3WordsKey'] = '${W3W_KEY}';
-    window['env']['isDemo'] = '${IS_DEMO}';
-    window['env']['demoToken'] = '${DEMO_TOKEN}';
-    window['env']['osmMapKey'] = '${OSM_MAP_KEY}';
-    window['env']['mapTilerKey'] = '${MAPTILER_KEY}';
-    window['env']['googleMapsKey'] = '${GOOGLE_MAPS_KEY}';
     window['env']['loggingKey'] = '${LOGGING_KEY}';
+    window['env']['apiSettingsConfigKey'] = '${APISETTINGS_KEY}';
   })(this);
